98b27c9c595cf75007b1100740ba3a160612bd90
WebmasterSensei <webmasteraturservice@gmail.com>
2025-01-15 17:57:58 +0800

    adding pictures

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -37,14 +37,14 @@ export default function Dashboard() {
                                                 <div className="relative">
                                                     <img
                                                         className="w-12 h-12 rounded-full border border-gray-300"
-                                                        src="https://i.pravatar.cc/150?img=1"
+                                                        src="https://imgs.search.brave.com/MA3s1z7gfsxb9WPXXf5sz4mHU_vLXh0DexuuFfAM500/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJzLWNsYW4u/Y29tL3dwLWNvbnRl/bnQvdXBsb2Fkcy8y/MDIyLzAyL2RlbW9u/LXNsYXllci10YW5q/aXJvLXBmcC0xLmpw/Zw"
                                                         alt="User 1"
                                                     />
                                                     <span className="absolute bottom-0 right-0 bg-green-500 h-3 w-3 rounded-full border border-white"></span>
                                                 </div>
                                                 <div className="ml-4">
                                                     <p className="text-sm font-medium text-gray-700">
-                                                        Jane Doe
+                                                        Tanjirou Kamado
                                                     </p>
                                                     <p className="text-xs text-gray-500">
                                                         Online
@@ -55,14 +55,14 @@ export default function Dashboard() {
                                                 <div className="relative">
                                                     <img
                                                         className="w-12 h-12 rounded-full border border-gray-300"
-                                                        src="https://i.pravatar.cc/150?img=2"
+                                                        src="https://imgs.search.brave.com/77uPSzhkPn5Jz4YQu_X0Z7n43shMznX8GiPolDp8W0U/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2U0LzEx/LzU4L2U0MTE1ODc3/ZTZhM2VkMDAxM2Zk/MWM4NWU4MzA3YmJj/LmpwZw"
                                                         alt="User 2"
                                                     />
                                                     <span className="absolute bottom-0 right-0 bg-green-500 h-3 w-3 rounded-full border border-white"></span>
                                                 </div>
                                                 <div className="ml-4">
                                                     <p className="text-sm font-medium text-gray-700">
-                                                        John Smith
+                                                        Sung Jin Woo
                                                     </p>
                                                     <p className="text-xs text-gray-500">
                                                         Online
@@ -73,14 +73,86 @@ export default function Dashboard() {
                                                 <div className="relative">
                                                     <img
                                                         className="w-12 h-12 rounded-full border border-gray-300"
-                                                        src="https://i.pravatar.cc/150?img=3"
+                                                        src="https://imgs.search.brave.com/iZLK26TiEr0U8Rx_CEvsfLNIM8QXnFJ7_A21TFyO09o/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/d2F0dHBhZC5jb20v/Y292ZXIvMjE0OTE0/MzY5LTUxMi1rMzgx/MjQ5LmpwZw"
                                                         alt="User 3"
                                                     />
                                                     <span className="absolute bottom-0 right-0 bg-green-500 h-3 w-3 rounded-full border border-white"></span>
                                                 </div>
                                                 <div className="ml-4">
                                                     <p className="text-sm font-medium text-gray-700">
-                                                        Alice Johnson
+                                                        Sakai Rihito
+                                                    </p>
+                                                    <p className="text-xs text-gray-500">
+                                                        Online
+                                                    </p>
+                                                </div>
+                                            </li>
+                                            <li className="flex items-center p-1">
+                                                <div className="relative">
+                                                    <img
+                                                        className="w-12 h-12 rounded-full border border-gray-300"
+                                                        src="https://imgs.search.brave.com/o9_TJsw_RM1WvEUjDo5hUuaviT2yF_UE2as_xJkcvh4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9waWNz/LmNyYWl5b24uY29t/LzIwMjQtMDktMDcv/MmVfSHpsN2FSaC1S/RWJCWXZiNElIdy53/ZWJw"
+                                                        alt="User 3"
+                                                    />
+                                                    <span className="absolute bottom-0 right-0 bg-green-500 h-3 w-3 rounded-full border border-white"></span>
+                                                </div>
+                                                <div className="ml-4">
+                                                    <p className="text-sm font-medium text-gray-700">
+                                                       Satoru Gojo
+                                                    </p>
+                                                    <p className="text-xs text-gray-500">
+                                                        Online
+                                                    </p>
+                                                </div>
+                                            </li>
+                                            <li className="flex items-center p-1">
+                                                <div className="relative">
+                                                    <img
+                                                        className="w-12 h-12 rounded-full border border-gray-300"
+                                                        src="https://imgs.search.brave.com/Avf7FP7kvj59VGnIpyULcZsdoeNBD0sqInFeMWXxcBM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2ZkLzFl/LzQ0L2ZkMWU0NDQ4/NmZhODg0ZDc1MGYw/ZWQxNGRjYWU4ZWEz/LmpwZw"
+                                                        alt="User 3"
+                                                    />
+                                                    <span className="absolute bottom-0 right-0 bg-green-500 h-3 w-3 rounded-full border border-white"></span>
+                                                </div>
+                                                <div className="ml-4">
+                                                    <p className="text-sm font-medium text-gray-700">
+                                                       Itadori Yuji
+                                                    </p>
+                                                    <p className="text-xs text-gray-500">
+                                                        Online
+                                                    </p>
+                                                </div>
+                                            </li>
+                                            <li className="flex items-center p-1">
+                                                <div className="relative">
+                                                    <img
+                                                        className="w-12 h-12 rounded-full border border-gray-300"
+                                                        src="https://imgs.search.brave.com/Jg-mYbUwjQaCu9w8vQ6y5_6ztg_l3tZ5j0w69XkK7Sk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJjYXZlLmNv/bS93cC93cDgyMTc1/NDcuanBn"
+                                                        alt="User 3"
+                                                    />
+                                                    <span className="absolute bottom-0 right-0 bg-green-500 h-3 w-3 rounded-full border border-white"></span>
+                                                </div>
+                                                <div className="ml-4">
+                                                    <p className="text-sm font-medium text-gray-700">
+                                                       Fushiguro Megumi
+                                                    </p>
+                                                    <p className="text-xs text-gray-500">
+                                                        Online
+                                                    </p>
+                                                </div>
+                                            </li>
+                                            <li className="flex items-center p-1">
+                                                <div className="relative">
+                                                    <img
+                                                        className="w-12 h-12 rounded-full border border-gray-300"
+                                                        src="https://imgs.search.brave.com/zjn5pPXdjuwFrwmK0G6e41dZp9-MoGu0z3T6NE2UP-Y/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzdhLzA0/LzFmLzdhMDQxZjk2/ZGNmZjc5NGY0OTI4/MjQ5NWQ5ZjQxMDEw/LmpwZw"
+                                                        alt="User 3"
+                                                    />
+                                                    <span className="absolute bottom-0 right-0 bg-green-500 h-3 w-3 rounded-full border border-white"></span>
+                                                </div>
+                                                <div className="ml-4">
+                                                    <p className="text-sm font-medium text-gray-700">
+                                                       Umaru Doma
                                                     </p>
                                                     <p className="text-xs text-gray-500">
                                                         Online
